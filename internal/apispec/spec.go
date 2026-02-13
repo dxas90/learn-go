d5f64b55db9d6933aa20474563aeeab6cbcc1a92
Daniel Ramirez <dxas90@gmail.com>
2026-02-13 23:17:18 +0200

    more lint

--- internal/apispec/spec.go
+++ internal/apispec/spec.go
@@ -1,6 +1,6 @@
 package apispec
 
-// _ imports embed package for the go:embed directive
+// _ imports embed package for the go:embed directive.
 import _ "embed"
 
 // OpenAPISpec contains the embedded OpenAPI specification file in YAML format.
