b672cd73ab8daf65eb3848adc80f050b1d4457ca
Daniel Ramirez <dxas90@gmail.com>
2026-02-13 17:04:52 +0200

    chore: add comments for clarity in OpenAPISpec definition

--- internal/apispec/spec.go
+++ internal/apispec/spec.go
@@ -1,6 +1,9 @@
 package apispec
 
+// Import embed for the go:embed directive below
 import _ "embed"
 
+// OpenAPISpec contains the embedded OpenAPI specification file in YAML format
+//
 //go:embed openapi.yaml
 var OpenAPISpec []byte
